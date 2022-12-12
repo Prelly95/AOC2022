be79809aebd913c2b9411b6ff1ec7471fcec2477
Pat Prell <prelly95@gmail.com>
2022-12-12 18:46:52 +1100

    finished day 6

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,8 +1,9 @@
+use std::collections::HashSet;
 use std::error::Error;
 use std::fs;
 
-const INPUT: &str = "./input/test.txt";
-// const INPUT: &str = "./input/input.txt";
+// const INPUT: &str = "./input/test.txt";
+const INPUT: &str = "./input/input.txt";
 
 type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 
@@ -17,10 +18,37 @@ fn main() -> Result<()> {
 }
 
 fn part1(input: &str) -> Result<()> {
-	println!("{}", input);
+	// let bit = input.chars().map(|x| x as u32).collect::<Vec<u32>>();
+	let bit = input.chars().collect::<Vec<char>>();
+	for ii in 0..bit.len() {
+		let mut marker = vec![];
+		for jj in 0..4 {
+			if !marker.contains(&bit[ii + jj]) {
+				marker.push(bit[ii + jj]);
+			}
+		}
+		if marker.len() >= 4 {
+			println!("First packet marker = {:?}, {}", marker, ii + 4);
+			break;
+		}
+	}
 	Ok(())
 }
+
 fn part2(input: &str) -> Result<()> {
-	println!("{}", input);
+	// let bit = input.chars().map(|x| x as u32).collect::<Vec<u32>>();
+	let bit = input.chars().collect::<Vec<char>>();
+	for ii in 0..bit.len() {
+		let mut marker = vec![];
+		for jj in 0..14 {
+			if !marker.contains(&bit[ii + jj]) {
+				marker.push(bit[ii + jj]);
+			}
+		}
+		if marker.len() >= 14 {
+			println!("First packet marker = {:?}, {}", marker, ii + 14);
+			break;
+		}
+	}
 	Ok(())
 }
